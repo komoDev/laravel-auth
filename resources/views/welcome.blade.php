0f5a42b4d227d8e21c214e867b22ebbd2ba14f99
komodo <komodo016@gmail.com>
2016-07-20 12:06:39 +0200

    app public page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Application's Public Page.
                 </div>
             </div>
         </div>
